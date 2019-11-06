bca65abfb6e8526ded6390ca9b8070c9d7109e88
zachwo <zacharywong880@outlook.com>
2019-11-06 19:42:19 +0800

    甲骨文培训班作业

--- new_20190519_task/src/com/oracle/task_1/Test.java
+++ new_20190519_task/src/com/oracle/task_1/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         Student stu = new Student("张三",19,"男");
         stu.showInfo();
+        System.out.println("hello github");
     }
 }
