036738976b583b4cbb5eeffcdb3bb430eb7c2932
zachwo <zacharywong880@outlook.com>
2019-12-02 14:57:26 +0800

    甲骨文培训班作业

--- 20190723_1/src/com/oracle/recycle/Person.java
+++ 20190723_1/src/com/oracle/recycle/Person.java
@@ -20,7 +20,7 @@ public class Person {
 
     /**
      * finalize
-     *该方法自jdk 9之后就被废弃了，之前的版本也不建议重写该Object方法实现某些功能
+     *注意⚠️！！！该方法自jdk 9之后就被废弃了，之前的版本也不建议重写该Object方法实现某些功能！！！
      * protected void finalize()
      *                  throws Throwable
      * 当垃圾回收器确定不存在对该对象的更多引用时，由对象的垃圾回收器调用此方法。子类重写 finalize 方法，以配置系统资源或执行其他清除。
---
+++ 20190723_2/src/com/oracle/date_class/Test.java
@@ -0,0 +1,56 @@
+package com.oracle.date_class;
+
+import javax.xml.transform.Source;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Test {
+    public static void main(String[] args) {
+        Date date = new Date();
+        // 等效于：
+        // Date date = new Date(System.currentTimeMillis());
+        System.out.println("默认date类对象的输出格式（当前时间）："+date);
+        System.out.println("date类对象起始时间："+new Date(1));
+        System.out.println();
+        //Calendar类,抽象类，需要子类去继承实现，或者直接通过子类的向上转型来创建对象
+        Calendar calendar = new GregorianCalendar();
+        System.out.println("获取当前年："+calendar.get(Calendar.YEAR));
+        System.out.println("获取当前月："+(calendar.get(Calendar.MONTH)+1)+" 注：国外的月份从0开始，所以转换为国内的习惯应该加1");
+        System.out.println("获取当前日："+calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println("获取当前星期几："+(calendar.get(Calendar.DAY_OF_WEEK)-1)+" 注：国外的一周从周日开始，所以转换为国内的习惯应该减1");
+        System.out.println("获取当前是这的月的第几周："+calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        System.out.println("获取当前日是今年的第几天："+calendar.get(Calendar.DAY_OF_YEAR));
+        System.out.println("获取时："+calendar.get(Calendar.HOUR));
+        System.out.println("获取时（24进制）："+calendar.get(Calendar.HOUR_OF_DAY));
+        System.out.println("获取分："+calendar.get(Calendar.MINUTE));
+        System.out.println("获取秒："+calendar.get(Calendar.SECOND));
+        System.out.println("获取毫秒："+calendar.get(Calendar.MILLISECOND));
+        System.out.println();
+        //SimpleDateFormate类，格式转换
+        Date date1 = new Date();
+        System.out.println("Date原始输出格式："+date1);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒 SSS毫秒");
+        // 把date对象转换为字符串：
+        String str = simpleDateFormat.format(date1);
+        System.out.println(str);
+        System.out.println();
+        // 把字符串转换为默认的date对象:
+        String str1 = "2001-01-01";
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date2 = simpleDateFormat1.parse(str1);
+            System.out.println(date2);
+            //再转换为自定义的格式：
+            SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+            String str2 = simpleDateFormat2.format(date2);
+            System.out.println(str2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
