bbe71f59bef4545494fd1cb1921f5e686e1ae9f2
zachwo <zacharywong880@outlook.com>
2019-12-05 13:11:21 +0800

    Oracle WDP日常练习更新

--- new_20190724_task/src/com/oracle/task6/Test.java
+++ new_20190724_task/src/com/oracle/task6/Test.java
@@ -1,7 +1,16 @@
 package com.oracle.task6;
 
+import java.io.File;
+
 /**
  * 6.写一个方法,可以复制一个目录,(此目录不为空，不要求复制文件的内容）)
  */
 public class Test {
+    public static void main(String[] args) {
+        String basePath = "/Users/wangze/JavaWorkspace/CodeDemo/new_20190724_task/src/file_practice";
+        String filePath = basePath+ File.separator+"task6";
+        new File(filePath).mkdirs();
+
+        File file = new File(filePath+File.separator+"directory1");
+    }
 }
