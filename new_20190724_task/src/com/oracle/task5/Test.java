4afb4910b4e1a4efcd563d0deb6482efeddbd7be
zachwo <zacharywong880@outlook.com>
2019-12-05 13:08:19 +0800

    Oracle WDP日常练习更新

--- new_20190724_task/src/com/oracle/task5/Test.java
+++ new_20190724_task/src/com/oracle/task5/Test.java
@@ -1,7 +1,25 @@
 package com.oracle.task5;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 5.写一个方法,可以复制一个文件（不要求复制文件的内容）
  */
 public class Test {
+    public static void main(String[] args) {
+        String basePath = "/Users/wangze/JavaWorkspace/CodeDemo/new_20190724_task/src/file_practice";
+        String filePath = basePath+ File.separator+"task5";
+        new File(filePath).mkdirs();
+
+        File file1 = new File(filePath+ File.separator+"a.txt");
+        try {
+            file1.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file2 = new File(filePath+File.separator+"a(copy).txt");
+        
+    }
 }
--- new_20190724_task/src/com/oracle/task7/test.java
+++ new_20190724_task/src/com/oracle/task7/test.java
@@ -1,7 +1,29 @@
 package com.oracle.task7;
 
+import java.io.File;
+
 /**
  * 7.写一个方法,可以将一个非空目录中的所有文件的层次显示出来；
  */
 public class test {
+    public static void main(String[] args) {
+        String basePath = "/Users/wangze/JavaWorkspace/CodeDemo/new_20190724_task/src/file_practice";
+        String filePath = basePath+ File.separator+"task7";
+        new File(filePath).mkdirs();
+
+        File file = new File(filePath+File.separator+"directory1");
+        printFilePath(file);
+    }
+
+    public static void printFilePath(File file){
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+            for (int i=0;i<files.length;i++){
+                printFilePath(files[i]);
+            }
+            System.out.println(file.getAbsolutePath());
+        }else {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
 }
