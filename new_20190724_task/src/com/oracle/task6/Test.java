5b02ba9c456ab52ee88ac7ffeb2cb1151c1f4dee
zachwo <zacharywong880@outlook.com>
2019-12-05 22:48:09 +0800

    Oracle WDP日常练习更新

--- new_20190724_task/src/com/oracle/task6/Test.java
+++ new_20190724_task/src/com/oracle/task6/Test.java
@@ -1,6 +1,7 @@
 package com.oracle.task6;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * 6.写一个方法,可以复制一个目录,(此目录不为空，不要求复制文件的内容）)
@@ -13,14 +14,36 @@ public class Test {
 
         File srcFile = new File(filePath+File.separator+"directory1");
         File destFile = new File(filePath+File.separator+"destFile");
+        copyDirectory(srcFile,destFile);
     }
 
+    /**
+     * 此方法同样适用于task5
+     * @param srcFile
+     * @param destFile
+     */
     public static void copyDirectory(File srcFile,File destFile){
         if (srcFile.exists()){
             if (!destFile.exists()){
                 destFile.mkdirs();
             }
-            
+            if (srcFile.isDirectory()){
+                destFile = new File(destFile.getAbsolutePath()+File.separator+srcFile.getName());
+                destFile.mkdirs();
+
+                File[] files = srcFile.listFiles();
+                for (int i = 0;i<files.length;i++){
+                    copyDirectory(files[i],destFile);
+                }
+            }else {
+                String destpath = destFile.getAbsolutePath();
+                File file = new File(destpath+File.separator+srcFile.getName());
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
